c1d6bc47976fdf034fcb05b1b4df6e6505f28f4f
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 14:56:45 +0200

    Added ctor to RegularClient.cs

--- Kashkeshet.ServerImplementations/Clients/RegularClient.cs
+++ Kashkeshet.ServerImplementations/Clients/RegularClient.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Kashkeshet.ServerCore.Abstracts;
+using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Kashkeshet.ServerImplementations.Clients
 {
-    public class RegularClient
+    public class RegularClient : ClientBase
     {
+        public RegularClient(TcpClient client, string name) : base(client, name)
+        {
+        }
+
+        public override Task<bool> UpdateClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task HandleNewOrder(byte[] data, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
