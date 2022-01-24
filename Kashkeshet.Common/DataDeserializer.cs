7ac6fd0dd80641768625d9b85c0394c6c971544b
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 16:40:37 +0200

    Added DataDeserializer

---
+++ Kashkeshet.Common/DataDeserializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Text;
+
+namespace Kashkeshet.Common
+{
+    public class DataDeserializer
+    {
+        public byte[] Deserialize(JsonObject objectToDeserialzie)
+        {
+            //Todo: Implement;
+            return new byte[] { };
+        }
+    }
+}
