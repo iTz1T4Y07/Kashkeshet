6f037b38f9ab8afc42ef5d0c10c5a5b6a38f6a78
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 15:06:20 +0200

    Added Kashkeshet.Client project

---
+++ Kashkeshet.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kashkeshet.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
