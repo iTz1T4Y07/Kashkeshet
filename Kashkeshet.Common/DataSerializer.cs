729caed204419335a96e89caa1a86137f540b8cc
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 16:39:16 +0200

    Added DataSerializer

---
+++ Kashkeshet.Common/DataSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Text;
+
+namespace Kashkeshet.Common
+{
+    public class DataSerializer
+    {
+        public JsonObject Serialize(byte[] data)
+        {
+            //TODO: Implement
+            return new JsonObject();
+        }
+    }
+}
