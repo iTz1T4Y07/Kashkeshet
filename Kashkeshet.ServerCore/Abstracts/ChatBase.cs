ade91cfc25ff192780c2ebd578a306d7127daf4b
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 14:53:11 +0200

    Implemented ChatBase.cs

--- Kashkeshet.ServerCore/Abstracts/ChatBase.cs
+++ Kashkeshet.ServerCore/Abstracts/ChatBase.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Kashkeshet.ServerCore.Abstracts
 {
-    public class ChatBase
+    public abstract class ChatBase
     {
+        public readonly Guid id = Guid.NewGuid();
+        protected ISet<ClientBase> ConnectedClients;
+
+        public ChatBase(ISet<ClientBase> clients)
+        {
+            ConnectedClients = clients;
+        }
+
+        public bool TryAddClient(ClientBase client) => ConnectedClients.Add(client);
+
+        public bool TryRemoveClient(ClientBase client) => ConnectedClients.Remove(client);
+
+        public abstract Task UpdateAllClients();
     }
 }
