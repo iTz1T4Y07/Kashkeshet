02c7716566dc8f0bf2459183c7dd4c7139b32878
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 15:31:11 +0200

    Added ChatBase.cs

--- Kashkeshet.Common/IMessage.cs
+++ Kashkeshet.Common/IMessage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Kashkeshet.Common
-{
+{    
     public interface IMessage<T>
     {
         public MessageType Type { get; }
---
+++ Kashkeshet.LogicBll/Abstracts/ChatBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.LogicBll.Abstracts
+{
+    public class ChatBase
+    {
+    }
+}
