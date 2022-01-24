069ba81f01c399e887d6872809687bbce32da9f4
iTz1T4Y <itz1t4y@gmail.com>
2022-01-24 18:41:58 +0200

    Implemented CommandHandler

--- Kashkeshet.ConsoleUI/CommandHandler.cs
+++ Kashkeshet.ConsoleUI/CommandHandler.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Kashkeshet.NetworkBll;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Kashkeshet.ConsoleUI
 {
     public class CommandHandler
     {
+        private ServerCommunicator _communicator;
+
+        public Task HandleNewCommand()
+        {
+            return Task.Run(() => { }) ;
+        }
     }
 }
