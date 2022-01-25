c9c22f866301630b44c7e0d7ba6bc3546b639212
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 14:33:17 +0200

    Added ClientOrderHandler.cs & implemented it

---
+++ Kashkeshet.ServerCore/ClientOrderHandler.cs
@@ -0,0 +1,31 @@
+﻿using Kashkeshet.Common;
+using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.ServerCore
+{
+    public class ClientOrderHandler
+    {
+        public event Func<Guid, Message, Task> AddMessageToChat;
+
+        public async Task HandleOperation(Operation operation, JsonObject arguments)
+        {
+            if (operation == Operation.SendMessage)
+            {
+                if (!arguments.ContainsKey("chat_id") || !arguments.ContainsKey("message"))
+                {
+                    return;
+                }
+
+                Guid chatId = arguments["chat_id"];
+                Message message = JsonSerializer.Deserialize<Message>(arguments["message"]);
+                await AddMessageToChat?.Invoke(chatId, message);
+            }
+        }
+
+    }
+}
