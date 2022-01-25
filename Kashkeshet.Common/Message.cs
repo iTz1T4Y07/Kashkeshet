8076325107e6af99b0928f9ea317ef3f82ab6522
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 12:22:32 +0200

    Added SenderId to Message

--- Kashkeshet.Common/Message.cs
+++ Kashkeshet.Common/Message.cs
@@ -7,7 +7,15 @@ namespace Kashkeshet.Common
     [Serializable]
     public class Message
     {
+        public Guid SenderId { get; }
         public MessageType Type { get; }
-        public byte[] messageData { get; }
+        public byte[] MessageData { get; }
+
+        public Message(Guid senderId, MessageType type, byte[] messageData)
+        {
+            SenderId = senderId;
+            Type = type;
+            MessageData = messageData;
+        }
     }
 }
