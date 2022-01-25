71702383c06ba655c99b6a6b7be7abc9e26cea54
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 17:36:26 +0200

    Implemented Boostrapper.GetServer()

--- Kashkeshet.Server/Bootstrapper.cs
+++ Kashkeshet.Server/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kashkeshet.ServerCore.Abstracts;
+using Kashkeshet.ServerImplementations.Chats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,12 @@ namespace Kashkeshet.Server
 {
     public class Bootstrapper
     {
+        public Server GetServer()
+        {
+            IList<ChatBase> chats = new List<ChatBase>();
+            chats.Add(new PublicChat(new HashSet<ClientBase>()));
+            Server server = new Server(9090, chats);
+            return server;
+        }
     }
 }
