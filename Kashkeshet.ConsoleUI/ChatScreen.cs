3fa70513f7f7cef5dd967a5f7be54eec8f621e64
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 11:07:52 +0200

    Implemented ctor & Start()

--- Kashkeshet.ConsoleUI/ChatScreen.cs
+++ Kashkeshet.ConsoleUI/ChatScreen.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Kashkeshet.LogicBll;
+using Kashkeshet.NetworkBll;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace Kashkeshet.ConsoleUI
 {
     public class ChatScreen
     {
+        public readonly Guid Id;
+        protected ServerCommunicator _communicator;
+        protected ChatUpdater _updater;
+        protected Process _windowProcess;
+        protected StreamWriter _writer;
+        protected StreamReader _reader;
+
+        public ChatScreen(Guid chatId, ServerCommunicator communicator, ChatUpdater updater)
+        {
+            Id = chatId;
+            _communicator = communicator;
+            _updater = updater;            
+        }
+
+        public void Start()
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                FileName = $"Chat ID {Id}"
+            };
+
+            _windowProcess = Process.Start(processStartInfo);
+            _writer = _windowProcess.StandardInput;
+            _reader = _windowProcess.StandardOutput;
+        }
     }
 }
---
+++ Kashkeshet.ConsoleUI/ConsoleScreen.cs
@@ -0,0 +1,30 @@
+﻿using Kashkeshet.LogicBll;
+using Kashkeshet.NetworkBll;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.ConsoleUI
+{
+    public class ConsoleScreen
+    {
+        private ChatInformationExtractor _informationExtractor;
+        private Guid _activeChatId;
+        private ServerCommunicator _serverCommunicator;
+        private CommandHandler _commandHandler;
+
+        public ConsoleScreen(ChatInformationExtractor informationExtractor, ServerCommunicator communicator, CommandHandler commandHandler)
+        {
+            _informationExtractor = informationExtractor;
+            _serverCommunicator = communicator;
+            _commandHandler = commandHandler;
+        }
+
+        public Task WaitForNewCommand()
+        {            
+            string input = Console.ReadLine();
+            _commandHandler.HandleNewCommand(input);
+        }
+    }
+}
