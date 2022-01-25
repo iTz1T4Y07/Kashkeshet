3b6cf7a3349734845d5dc7bde9ffa9cd2cef6495
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 16:47:46 +0200

    Added StandardChat.cs

---
+++ Kashkeshet.LogicBll/StandardChat.cs
@@ -0,0 +1,15 @@
+﻿using Kashkeshet.Common;
+using Kashkeshet.LogicBll.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.LogicBll
+{
+    public class StandardChat : ChatBase
+    {
+        public StandardChat(Guid id, IList<Message> messages, IDictionary<Guid, string> clients) : base(id, messages, clients)
+        {
+        }
+    }
+}
