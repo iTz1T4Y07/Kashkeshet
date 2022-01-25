15263dff3008c8f492d1ed85bf8f2ba58a9ffc13
iTz1T4Y <itz1t4y@gmail.com>
2022-01-25 12:57:46 +0200

    Implemented ctor of ConsoleScreen

--- Kashkeshet.ConsoleUI/ConsoleScreen.cs
+++ Kashkeshet.ConsoleUI/ConsoleScreen.cs
@@ -9,16 +9,22 @@ namespace Kashkeshet.ConsoleUI
 {
     public class ConsoleScreen
     {
+        private ChatScreen _currentChat;
         private ChatInformationExtractor _informationExtractor;
-        private Guid _activeChatId;
-        private ServerCommunicator _serverCommunicator;
+        private ServerCommunicator _serverCommunicator;        
         private CommandHandler _commandHandler;
 
-        public ConsoleScreen(ChatInformationExtractor informationExtractor, ServerCommunicator communicator, CommandHandler commandHandler)
+        public ConsoleScreen(ChatInformationExtractor informationExtractor, ServerCommunicator communicator, CommandHandler commandHandler, ChatUpdater updater)
         {
             _informationExtractor = informationExtractor;
             _serverCommunicator = communicator;
             _commandHandler = commandHandler;
+            Guid chatId = informationExtractor.GetMainChatId();
+            if (chatId == Guid.Empty)
+            {
+                throw new OperationCanceledException("Found 0 chats available.");
+            }
+            _currentChat = new ChatScreen(chatId , informationExtractor, updater);
         }
 
         public Task WaitForNewCommand()
